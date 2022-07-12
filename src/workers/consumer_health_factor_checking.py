c6e47a61fa3aa258a37a9ebadec1242a3be1dd25
Pham Anh Tai <phamanhtai0410@gmail.com>
2022-07-12 14:53:12 +0700

    _stag_4_ add try except for worker

--- src/workers/consumer_health_factor_checking.py
+++ src/workers/consumer_health_factor_checking.py
@@ -21,37 +21,41 @@ from src.task import worker
 
 
 def on_message_health_factor(channel, method, properties, body):
-    msg = body.decode("utf8")
-    msg = json.loads(msg)
-    _data_msg = msg
-    # Log received message
-    LoggerTask.debug(_data_msg)
-    # parse message
-    _user_public_address = get(_data_msg, "address").lower()
-    _user_id = get(_data_msg, 'user')
-    _args = get(_data_msg, "returnValues")
-    _old_hf = get(_data_msg, 'old_hf')
-    _new_hf = get_health_factor(_user_public_address)
+    try:
+        msg = body.decode("utf8")
+        msg = json.loads(msg)
+        _data_msg = msg
+        # Log received message
+        LoggerTask.debug(_data_msg)
+        # parse message
+        _user_public_address = get(_data_msg, "address").lower()
+        _user_id = get(_data_msg, 'user')
+        _args = get(_data_msg, "returnValues")
+        _old_hf = get(_data_msg, 'old_hf')
+        _new_hf = get_health_factor(_user_public_address)
 
-    print('New HF = ', _new_hf)
+        print('New HF = ', _new_hf)
 
-    _tx = UsersModel.db().find_one_and_update(
-        filter={
-            "_id": ObjectId(_user_id)
-        },
-        update=[{
-            "$set": {
-                "hf": _new_hf,
-                "updated_time": dt_utcnow(),
-                "created_time": {"$cond": [{"$not": ["$created_time"]}, dt_utcnow(), "$created_time"]},
-            }
-        }],
-        upsert=True
-    )
-    if not _tx:
-        return f"Reject tx {_tx}"
+        _tx = UsersModel.db().find_one_and_update(
+            filter={
+                "_id": ObjectId(_user_id)
+            },
+            update=[{
+                "$set": {
+                    "hf": _new_hf,
+                    "updated_time": dt_utcnow(),
+                    "created_time": {"$cond": [{"$not": ["$created_time"]}, dt_utcnow(), "$created_time"]},
+                }
+            }],
+            upsert=True
+        )
+        if not _tx:
+            return f"Reject tx {_tx}"
+        channel.basic_ack(delivery_tag=method.delivery_tag)
 
-    channel.basic_ack(delivery_tag=method.delivery_tag)
+    except Exception as e:
+        print(e)
+        channel.basic_nack(delivery_tag=method.delivery_tag)
 
 
 def handle_msg(_cfg):
